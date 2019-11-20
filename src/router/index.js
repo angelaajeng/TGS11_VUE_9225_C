18188a6a811069a307eba141730d1dfc88fcd323
angelaajeng <angelaajeng04@gmail.com>
2019-11-20 18:20:21 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,60 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+const DashboardLayout = () => import(/*webpackChunkName:"dashboard"*/ '../components/dashboardLayout.vue')
+const LandingLayout = () => import('../components/landingLayout.vue')
+
+function loadView(view){
+    return () => import(/*webpackChunkName:"view-[request]"*/ `../components/dashboardContents/${view}.vue`)
+}
+
+function loadLanding(view) {
+    return () => import(`../components/landingContents/${view}.vue`)
+}
+
+const routes = [
+    {
+        path: '/',
+        component: LandingLayout,
+        children: [
+            {
+                name: 'LandingPage',
+                path: '',
+                component: loadLanding('landingPage')
+            }
+        ]
+    },
+    {
+        path: '/dashboard',
+        component: DashboardLayout,
+        children: [
+            {
+
+                name: 'login',
+                path: '/login',
+                component: loadView('login')
+
+            },
+            {
+
+                name: 'BengkelController',
+                path: '/bengkelController',
+                component: loadView('bengkelController')
+
+            },
+            {
+                name: 'UserController',
+                path: '/userController',
+                component: loadView('userController')
+            },
+
+        ]
+    },
+
+
+]
+Vue.use(Router)
+
+const router = new Router({mode: 'history', routes: routes})
+
+export default router
